fe51458b47a77ad13ce969de5c3831bcac304df9
lugh <lugh@winhong.com>
2020-02-26 19:32:06 +0800

    commit test

---
+++ test/src/main/java/com/test/dictionary/Dictionary.java
@@ -0,0 +1,18 @@
+package com.test.dictionary;
+
+public class Dictionary {
+	
+	private String[] dict = {"i","like","sam","sung","samsung","mobile","ice","cream","and","man","go"};
+
+	public Dictionary() {
+		super();
+	}
+
+	public void setDict(String[] dict) {
+		this.dict = dict;
+	}
+
+	public String[] getDict(){
+		return dict;
+	}
+}
